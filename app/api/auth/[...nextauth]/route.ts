4a3bb7a5699622483f5a24a9aa12df74bede2ee5
Ahmed <ahmedzcy539@gmail.com>
2025-04-15 08:16:01 -0700

    Move auth providers from auth.config.ts to auth.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
-import { authConfig } from "@/lib/auth.config";
+import { authOptions } from "@/lib/auth";
 
 // Export a handler function to NextAuth that will handle all requests
-const handler = NextAuth(authConfig);
+const handler = NextAuth(authOptions);
 
 // Export GET and POST functions to handle authentication requests
 export { handler as GET, handler as POST }; 
